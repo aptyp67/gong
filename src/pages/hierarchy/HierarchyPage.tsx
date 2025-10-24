a8ffee121e4b27f497b03a21f2650064167cc3df
Artur Artur <arturartur@Arturs-MacBook-Air.local>
2025-10-24 12:42:24 +0100

    add routing, basic styling, remove boilerplate

---
+++ src/pages/hierarchy/HierarchyPage.tsx
@@ -0,0 +1,8 @@
+export function HierarchyPage() {
+  return (
+    <section>
+      <h1>Hierarchy Tree</h1>
+      <p>Hierarchy tree goes here.</p>
+    </section>
+  )
+}
---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,8 @@
+export function LoginPage() {
+  return (
+    <section>
+      <h1>Please login</h1>
+      <p>Form goes here.</p>
+    </section>
+  )
+}
